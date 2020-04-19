6b0759dac3579125fbfde31f2d223290699cc42a
Zeus <63179943+Zeus250@users.noreply.github.com>
2020-04-19 15:06:19 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-const http = require("http");
-const express = require("express");
-const queue = new Map();
+const http = require('http');
+const express = require('express');
 const app = express();
+const queue = new Map();
+const Discord = require("discord.js");
+const fs = require("fs");
+const superagent = require("superagent");
+const request = require("request");
+const { registerFont } = require("canvas");
+const { Canvas } = require("canvas-constructor");
+const canvas  = require("canvas");
+const snekfetch = require("snekfetch");
+const sql = require("sqlite");
+const fetch = require("node-superfetch");
+const client = new Discord.Client({ disableEveryone: true });
+client.login(process.env.TOKEN);
+const config = require("./config.json");
 
 app.get("/", (request, response) => {
   console.log(Date.now() + " Ping ");
@@ -12,114 +25,96 @@ setInterval(() => {
   http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
 }, 280000);
 
-const Discord = require("discord.js");
-const fs = require("fs");
-const superagent = require("superagent");
-const request = require("request");
-const { registerFont } = require('canvas')
-const { Canvas } = require("canvas-constructor");
-const snekfetch = require("snekfetch");
-const sql = require('sqlite');
-const fetch = require("node-superfetch");
-const client = new Discord.Client({disableEveryone: true});
-client.login(process.env.TOKEN);
-const config = require("./config.json");
+
 
 client.on("ready", async () => {
-console.log(`Bots is ready and working in ${client.guilds.size} servers with ${client.users.size} users!`);
-});
-  
-
-client.on('ready', async member => {  
-     try {
-       let link = await client.generateInvite(["ADMINISTRATOR"]);
-       console.log(link);
-   } catch(e) {
-       console.log(e.stack);
-   }
-  
-    client.user.setStatus("Online");
-  
-
-client.on('message', message => {
-  if (message.content === '-test') {
-    client.emit('guildMemberAdd', message.member);
-  } 
+  console.log(
+    `Bots is ready and working in ${client.guilds.size} servers with ${client.users.size} users!`
+  );
 });
 
-client.on("guildMemberAdd", async member => {
-  let welcome = JSON.parse(fs.readFileSync('./database/welcome.json','utf8'))
-  let welcomeset = JSON.parse(fs.readFileSync('./database/welcomeset.json','utf8'))
-    if(!welcomeset[member.guild.id]){
-    welcomeset[member.guild.id] = {
-    checker : 0 };
-};
-  let value = welcomeset[member.guild.id].checker;
-    if(value === undefined) return;
-    if(value === 0) return;
-    if(value === 1) {
-    let channelid = welcome[member.guild.id].role;
-    const channel =  member.guild.channels.find( ch => ch.id === channelid);
-    if (!channel) return;
-      
-//SET BACKGROUND 
-    let defaultback = 'https://cdn.nekos.life/wallpaper/E6spqhuB1vc.jpg';
-    let background = JSON.parse(fs.readFileSync('./database/backgroundwelcome.json', 'utf8'));
-  
-    if(!background[member.guild.id]){
-      background[member.guild.id] = {
-        backgrounds: defaultback
-    };
-  };
-    let backimg = background[member.guild.id].backgrounds;
-      
-      
-    let nameLimit = member.user.username;
-    let username = nameLimit.length > 25 ? nameLimit.substring(0, 23) + "" : nameLimit;
-    async function createCanvas() {
-            let { body : background } = await superagent.get(backimg);//("https://cdn.discordapp.com/attachments/596041860711972864/665779128598790144/images_22.jpeg");
-            let avatar;
-            try {
-              avatar = await superagent.get(member.user.displayAvatarURL);
-            } catch (e) {
-              if (e.message == 'Forbidden') {
-                avatar = await superagent.get('https://cdn.discordapp.com/embed/avatars/0.png');
-              }
-            }
-      
-           
-            return new Canvas(1000, 500)//(1024, 450)
-              .addImage(background, 0, 0, 1000, 500) // BACKGROUND
-              .setShadowColor("rgba(22, 22, 22, 1)")
-              .setShadowOffsetY(5)
-              .setShadowBlur(10)
-              .setColor(`white`)
-              .addCircle(500, 160, 125)
-              .addCircularImage(avatar.body, 500 , 160, 120) // PROFILE
-     
-      
-              .setTextFont('bold 40px sans-serif')
-              .setTextAlign('center')
-              .addText(`${username}#${member.user.discriminator}`, 500, 435)//385)
-               
-              .setTextFont('bold 75px sans-serif')
-              .setTextAlign('center')
-              .addText("WELCOME", 500, 385)//345)
-              
-              .setTextFont('bold 30px sans-serif')
-              .setTextAlign('center')
-              .addText(`${member.guild.name}`, 500, 475)//430)
-              .toBufferAsync()
+client.on("ready", async member => {
+  try {
+    let link = await client.generateInvite(["ADMINISTRATOR"]);
+    console.log(link);
+  } catch (e) {
+    console.log(e.stack);
+  }
+
+  client.user.setStatus("Online");
+
+  //WELCOME IMAGE ==================================================================================================================
+  client.on("message", message => {
+    if (message.content === ">test") {
+      client.emit("guildMemberAdd", message.member);
+    }
+  });
+
+  client.on("guildMemberAdd", async member => {
+    let welcome = JSON.parse(fs.readFileSync("./database/welcome.json", "utf8"));
+    let welcomeset = JSON.parse(fs.readFileSync("./database/welcomeset.json", "utf8"));
+    if (!welcomeset[member.guild.id]) {
+      welcomeset[member.guild.id] = {
+        checker: 0
       };
-      let Channel = member.guild.channels.get("channelid");
+    }
+    let value = welcomeset[member.guild.id].checker;
+    if (value === undefined) return;
+    if (value === 0) return;
+    if (value === 1) {
+      let channelid = welcome[member.guild.id].role;
+      const channel = member.guild.channels.cache.find(ch => ch.id === channelid);
       if (!channel) return;
-    channel.send({
-    files: [{
-    attachment: await createCanvas(),
-    name: 'welcome.png'}] 
-  });    
-};
-});
+
+      //SET BACKGROUND ===============================================================================================================================
+      let defaultback ="https://cdn.discordapp.com/attachments/680027253756657683/693064245138685982/dv2LnYdWxqU.png";
+      let background = JSON.parse(fs.readFileSync("./database/backgroundwelcome.json", "utf8"));
+
+      if (!background[member.guild.id]) {
+        background[member.guild.id] = {
+          backgrounds: defaultback
+        };
+      }
+      let backimg = background[member.guild.id].backgrounds;
+      
+      let nameLimit = member.user.username;
+      let username = nameLimit.length > 25 ? nameLimit.substring(0, 23) + "" : nameLimit;
+      
+      async function createCanvas() {
+        let { body: background } = await superagent.get(backimg); 
+        let { body: avatar } = await superagent.get(member.user.displayAvatarURL({ format: 'png', dynamic: true, size: 1024 }));
+
+        return new Canvas(1000, 500) //(1024, 450)
+            .addImage(background, 0, 0, 1000, 500) // BACKGROUN
+            .setShadowColor("rgba(22, 22, 22, 1)")
+            .setShadowOffsetY(5)
+            .setShadowBlur(10)
+            .setColor(`${color}`)
+            .addCircle(150, 160, 125)
+            .addCircularImage(avatar, 150, 160, 120) // PROFILE
+        
+            .setTextFont("bold 40px sans-serif'")
+            .setTextAlign("center")
+            .addText(`${username}#${member.user.discriminator}`, 500, 435) 
+
+            .setTextFont("bold 75px sans-serif")
+            .setTextAlign("center")
+            .addText("WELCOME", 500, 385) 
+
+            .setTextFont("bold 30px sans-serif")
+            .setTextAlign("center")
+            .addText(`${member.guild.name}`, 500, 475) 
+            .toBufferAsync()
+      }
+  
+      channel.send(msg, {
+        files: [{
+            attachment: await createCanvas(),
+            name: "welcome.png"
+          }]
+      });
+    };
+  });
 });
 
 client.on("message", async message => {
